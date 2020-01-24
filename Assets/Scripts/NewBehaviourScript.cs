b16c4f21cdef70028ea571b0544080caf120cd21
Shad150 <cgpablo150@gmail.com>
2020-01-24 19:37:51 +0100

    Flappy movement and tubes

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    private Rigidbody2D myRigidbody2D;
+    [Range(3,8)]
+    public float speed = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.velocity = Vector2.up * speed;       //Vector2.up es el equivalente al eje y. Lo multiplico  por la velocidad.
+        }
+
     }
 }
