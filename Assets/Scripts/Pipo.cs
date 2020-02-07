c2d109167cba289f73fac82c6ff1087b313254e1
º <>
2020-02-07 19:38:19 +0100

    Animations

--- Assets/Scripts/Pipo.cs
+++ Assets/Scripts/Pipo.cs
@@ -10,24 +10,29 @@ public class Pipo : MonoBehaviour
     private Rigidbody2D myRigidbody2D;
     [Range(3,8)]
     public float speed = 4f;
+
     public Text scoreText;
     private int score = 0;
+
+    private int highscore = 0;
+    public Text highscoreText;
+
     public Transform lookAt;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
+
+        highscore = PlayerPrefs.GetInt("highScore");
+        highscoreText.text = "HIGHSCORE: " + highscore.ToString();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            myRigidbody2D.velocity = Vector2.up * speed;       //Vector2.up es el equivalente al eje y. Lo multiplico  por la velocidad.
-        }
+       
 
     }
 
@@ -35,6 +40,13 @@ public class Pipo : MonoBehaviour
     {
         string sceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(sceneName);
+
+        PlayerPrefs.SetInt("score", score);
+
+        if (PlayerPrefs.GetInt("highScore") < score)
+        {
+            PlayerPrefs.SetInt("highScore", score);
+        }
         
     }
 
@@ -42,10 +54,18 @@ public class Pipo : MonoBehaviour
     {
         score ++;
         scoreText.text = score.ToString();      //.ToString() para convertirlo en texto
+
+        
     }
 
     private void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.velocity = Vector2.up * speed;       //Vector2.up es el equivalente al eje y. Lo multiplico  por la velocidad.
+        }
+
         Vector3 targetPosition = lookAt.position;
         targetPosition.x = targetPosition.x - transform.position.x;
         targetPosition.y = targetPosition.y - transform.position.y;
